7c21036113cda181bcc121f2a8055abc2b5085ab
Ruffiuss <ruffiuss123@gmail.com>
2022-04-10 20:09:29 +0500

    Created RX abstraction over ScriptableValue

--- Assets/Code/UserControlSystem/UI/Model/AttackableValue.cs
+++ Assets/Code/UserControlSystem/UI/Model/AttackableValue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace UserControlSystem.UI.Model
 {
     [CreateAssetMenu(fileName = nameof(AttackableValue), menuName = "Strategy Game/" + nameof(AttackableValue), order = 0)]
-    public sealed class AttackableValue : ScriptableObjectValueBase<IAttackable>
+    public sealed class AttackableValue : StatefulSOValueBase<IAttackable>
     {
 
     }
